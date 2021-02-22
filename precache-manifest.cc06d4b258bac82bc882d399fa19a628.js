d86e050818ec83943678388cafed7fd959b97ae4
DebasmitBiswal <debasmitbiswal@outlook.com>
2021-02-22 08:27:37 +0530

    Updates

---
+++ precache-manifest.cc06d4b258bac82bc882d399fa19a628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8863fa44873d54d520b43dcc191a23a7",
+    "url": "/name/index.html"
+  },
+  {
+    "revision": "0ce9d5ed0d3efb304ef3",
+    "url": "/name/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/name/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/name/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce9d5ed0d3efb304ef3",
+    "url": "/name/static/js/main.c3171713.chunk.js"
+  },
+  {
+    "revision": "1d60664164ab73395e50",
+    "url": "/name/static/js/runtime-main.1c142bee.js"
+  }
+]);
